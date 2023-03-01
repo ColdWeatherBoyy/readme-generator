09fcb78b4a2048e0abe077149f3c92952c2f424c
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-01 15:39:18 -0500

    adds initial questions

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "projectitle"
+  },
+  {
+    type: "input",
+    message: "Provide a detailed description of your project, please",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Please provide installation instructions.",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "Provide usage information, please.",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Any guidelines for contribution?",
+    name: "contribution"
+  },
+  {
+    type: "input",
+    message: "Please list anyone who has contributed so far, and any other credits you want to include.",
+    name: "credits"
+  },
+  {
+    type: "input",
+    message: "Provide test information, please, as well?",
+    name: "test"
+  },
+  {
+    type: "list",
+    message: "Select the license you are using",
+    name: "license",
+    choices: ["MIT", "Apache License 2.0", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0"]
+  }
+  
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
